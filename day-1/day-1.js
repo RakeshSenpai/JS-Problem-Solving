b01c08d26443a83bc8155c4eca47fb0d64321f48
Rakesh07778777 <bossrakesh43@gmail.com>
2026-03-30 01:12:45 +0530

    tables

--- day-1/day-1.js
+++ day-1/day-1.js
@@ -8,7 +8,7 @@ for (let i = 0; i < parent.children.length; i++){
 }
 
 parent.children[2].style.color = 'grey'
-*/
+
 const parent = document.querySelector('.parent')
 const body = document.querySelector('body')
 
@@ -20,5 +20,38 @@ div.setAttribute('class' , 'new-child')
 console.log(div.getAttribute('class'))
 const newDiv = document.querySelector('.new-child')
 newDiv.style.color = 'red'
+// div.removeAttribute('class', 'new-child')
+console.log(div)
+*/
 
 
+
+
+const btn = document.querySelector('.btn')
+
+function generateListItems(){
+    const tbl = document.createElement('table')
+    const tbody = document.createElement('tbody')
+
+    for(let i = 0; i < 2 ; i++){
+        const row = document.createElement('tr')
+
+        for(let j = 0 ; j < 2; j++){
+            const cell = document.createElement('td');
+            const texts = document.createTextNode(`cell in row ${i} , columns ${j}`);
+
+            cell.appendChild(texts);
+           row.appendChild(cell); 
+
+        }
+        tbody.appendChild(row);
+    }
+    tbl.appendChild(tbody);
+    document.body.appendChild(tbl);
+
+    tbl.setAttribute('border' , '2')
+}
+
+btn.addEventListener('click' ,() => {
+    generateListItems()
+})
