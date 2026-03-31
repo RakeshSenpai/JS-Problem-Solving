fc2db989a4a8c59e201db4887d17d1c9ea3a0c11
Rakesh07778777 <bossrakesh43@gmail.com>
2026-03-31 23:46:07 +0530

    calculator

--- day-3/methods.js
+++ day-3/methods.js
@@ -1,12 +1,32 @@
-const arr = [ 'Apple' , 'Banana', 'Mango']
+// Map
 
-function renderHtml(){
-    const listItems = arr.map((user) => {
-            const div = document.createElement('div')
-            div.innerText = user
-            div.style.color = 'white'
-            document.body.appendChild(div)
-    })
-}
+// const arr = [ 'Apple' , 'Banana', 'Mango']
 
-renderHtml()
+// function renderHtml(){
+//     const listItems = arr.map((user) => {
+//             const div = document.createElement('div')
+//             div.innerText = user
+//             div.style.color = 'white'
+//             document.body.appendChild(div)
+//     })
+// }
+
+// renderHtml()
+
+const prices = [100, 300, 500]
+// const calculate = prices.map(p => p * 0.9)
+// console.log(calculate)
+// console.log(prices)
+
+const inr = prices.map(p => `$${p}`)
+console.log(inr)
+
+const users = [
+  { first: "Rakesh", last: "Shil" }
+];
+
+const name = users.map(n => ({
+    ...n, Fullname: `${n.first} ${n.last}`
+}))
+console.log(name)
+
