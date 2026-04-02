8afbcacd0a3521eb2738c805e68e96f65e956934
Rakesh07778777 <bossrakesh43@gmail.com>
2026-04-02 23:44:39 +0530

    nested loops

---
+++ day-04/loops/nested-L.js
@@ -0,0 +1,6 @@
+  for (let i = 0; i < 3 ; i++){
+    // console.log('outer Loop' + i)
+    for(let j = 0 ; j < i; j++){
+        console.log(('outter Loop' + i), ('inner Loop' + j))
+    }
+  }
---
+++ projects/gambling-game/app.js
@@ -0,0 +1,10 @@
+// Steps
+// 1. Deposit some money
+//2. Determine numbers of line to be bet on
+// 3. Collect a bet ammount
+// 4. Spin the slot machine
+// 5. check if the user won
+// 6. Give the user their winnings
+// 7. Play again
+
+  
