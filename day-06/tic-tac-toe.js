b662a9b44d09897baede656b5852a62ab4509cf0
Rakesh07778777 <bossrakesh43@gmail.com>
2026-05-01 14:39:49 +0530

    row-col

--- day-06/tic-tac-toe.js
+++ day-06/tic-tac-toe.js
@@ -1,6 +1,20 @@
 const prompt = require("prompt-sync")()
 
+function makeMove(turn, baord){
+    while(true){
+    const row = prompt("Enter row: ")
+    const col = prompt("Enter col: ")
 
+    if(isNaN(row) || row < 1 || row > 3)console.log("Invalid row")
+    else if(isNaN(col) || col < 1 || col > 3)console.log("Invalid column")
+    else if(baord[row - 1][col - 1] !== " ")console.log("Invalid Position")
+        else{
+            baord[row - 1][col - 1] = turn
+            break;
+    }
+
+    }
+}
 
 const board = [
     [" ", " ", " "],
