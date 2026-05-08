ad6a4cac8100074339cd44712b3270a876985ca6
Rakesh07778777 <bossrakesh43@gmail.com>
2026-05-08 15:28:25 +0530

    complete

--- day-08/search.js
+++ day-08/search.js
@@ -5,15 +5,18 @@ const searchInput = document.querySelector('[data-search]')
 let users = [];
 
 searchInput.addEventListener('input' , e => {
-    const value = e.target.value
-
+    const value = e.target.value.toLowerCase()
+    users.forEach(user => {
+        const isVisible = user.name.toLowerCase().includes(value) || user.email.toLowerCase().includes(value)
+        user.element.classList.toggle("hide", !isVisible) 
+    })
 })
 
 
 fetch('https://jsonplaceholder.typicode.com/users')
 .then(res => res.json())
 .then(data => {
-    data.map(user => {
+   users =  data.map(user => {
         const card = userCardTemplate.content.cloneNode(true).children[0]
         const header = card.querySelector('[data-header]')
         const body = card.querySelector('[data-body]')
@@ -21,6 +24,6 @@ fetch('https://jsonplaceholder.typicode.com/users')
         body.textContent = user.email
         userCardContainer.append(card)
 
-        return{name : user.name, email: user.email, content: card}
+        return{name : user.name, email: user.email, element: card}
     })
 })
